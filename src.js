af879e39b34ecaecaf16c1928ab184b0fc06b746
Philip Young <philip.young.gunawan@gmail.com>
2016-12-25 01:38:51 +0700

    update to better workflow; ;

--- src.js
+++ src.js
@@ -1,32 +1,27 @@
-const $ = Rx.Observable;
-const app = document.querySelector('#app');
-const button = document.querySelector('#button');
-const userURL = 'https://jsonplaceholder.typicode.com/users';
-const genRandomNum = num => Math.floor(Math.random() * num) + 1;
+((w, d, Rx) => {
+  const application = document.querySelector('#app');
+  const userURL = 'https://jsonplaceholder.typicode.com/users';
+  const genRandomNum = num => Math.floor(Math.random() * num) + 1;
 
-const request = $.fromEvent(button, 'click')
+  const request = Rx.Observable.fromEvent(document, 'click')
   .mapTo(userURL)
   .map(url => `${url}/${genRandomNum(10)}`)
   .share();
 
-const response = request
-  .delay(300) // emulate fetching data from server
-  .flatMap(url => $.fromPromise(fetch(url)))
-  .flatMap(data => data.json())
+  const response = request
+  .switchMap(url =>
+    fetch(url)
+    .then(res => res.json())
+  )
   .map(user => `
     <h1>${user.name}</h1>
     <p>Email: ${user.email}</p>
     <p>Tel: ${user.phone}</p>
-  `)
-  .share();
+  `);
 
+  const event = request
+  .mapTo('loading...')
+  .merge(response);
 
-const loadingState = request
-  .mapTo(true)
-  .merge(response.mapTo(false));
-
-const event = $.combineLatest(loadingState, response, (loading, data) =>
-  (loading ? '<h1>loading data...<h1>' : data)
-);
-
-event.subscribe(html => (app.innerHTML = html));
+  event.subscribe(html => (application.innerHTML = html));
+})(window, document, Rx);
